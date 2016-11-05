f7ad2c84763a2d7ea8e1401c094e839aa59aebee
Brian McIlwain <bmcilw1@gmail.com>
2016-11-05 10:26:05 -0500

    Finished lab 9

--- Lab9/Prob1.s
+++ Lab9/Prob1.s
@@ -35,15 +35,144 @@ stackstart  equ  0x40000200  ; start of stack
 
 start
 	ldr sp, = stackstart	; set up stack pointer
+	; Configure	UART0 to	receive	and	transmit
 	bl UARTConfig	; initialize/configure UART0
-	
-loop
-	bl Recieve
-	bl Transmit		; send character to UART
-	cmp r0, #0		; null terminated?
-	bne loop		; continue if not a '0'
+	; Output	"Enter	A:		0x"
+	ldr r1, =msgA
+	bl Write
+	; Receive	user	input	of	a	number in	hexadecimal
+	ldr r1, =hexA
+	bl Read
+	; Convert	the	entered	number	to	binary
+	bl toBinary
+	mov r2, r0 ; save binary number A to r2
+	; Output	"Enter	B:		0x"
+	ldr r1, =msgB
+	bl Write
+	; Receive	user input	of	a	number in	hexadecimal
+	ldr r1, =hexB
+	bl Read
+	; Convert	the	entered	number	to	binary
+	bl toBinary
+	; Compute	A +	B
+	add r0, r0, r2 ; r0 = A + B
+	; Output	"A	+	B	=		0x"
+	ldr r1, =msgAdd
+	bl Write
+	; Convert	the hexadecimal	representation	of	this sum to	ASCII
+	ldr r1, =hexSum
+	bl toAsciiArr
+	; Output	the	ASCII	form	of	the	sum
+	bl Write
 done	b done			; otherwise, we are done
 
+; subroutine toAsciiChar
+;   converts an input hex char to ascii
+;   Registers used:
+;   inputs:  r2 - hex char
+;   outputs:  r2 - ascii char
+
+toAsciiChar
+	push{lr}
+	cmp r2, #9 ; highest number
+	addle r2, r2, #48 ; convert 0-9 to char 0-9
+	addgt r2, r2, #55 ; convert A-F to char A-F
+	pop {pc}
+	
+; subroutine toAsciiArr
+;   converts a binary string to ascii hex array
+;   Registers used:
+; 	r2 - scratch
+;	r3 - scratch counter
+;   inputs:  r1 - start of ascii array
+;			 r0 - hex string
+;   outputs:  none
+
+toAsciiArr
+	push{r0 - r3, lr}
+	mov r3, #0
+asciiArrLoop
+	mov r2, #0
+	add r2, r0, lsr #28 ; r2 gets most significant hex of r0
+	lsl r0, #4 ; prep next hex
+	bl toAsciiChar ; convert r2 to ascii version of char
+	strb r2, [r1], #1	; store character, increment address
+	add r3, r3, #1 ; increment counter
+	cmp r3, #8 ; finished storing array?
+	blt asciiArrLoop		; continue if not finished
+	mov r0, #0
+	strb r0, [r1]	; store null terminator
+	pop {r0 - r3, pc}
+
+; subroutine toBinary
+;   converts an input char to hex
+;   Registers used:
+; 	r2 - scratch
+;   inputs:  r1 - start of ascii array
+;   outputs:  r0 - binary number
+
+toBinary
+	push{r1, r2, lr}
+	mov r2, #0 ; Zero r2
+binLoop	
+	ldrb r0, [r1], #1	; load character, increment address
+	cmp r0, #0		; null terminated?
+	blne toHex		; convert to hex/binary
+	lslne r2, r2, #4	; make room for next hex digit
+	addne r2, r2, r0  ; add new bit onto end of string
+	bne binLoop		; continue if not a '0'
+	mov r0, r2		; send output to r0
+	pop {r1, r2, pc}
+
+; subroutine toHex
+;   converts an input char to hex
+;   Registers used:
+;   inputs:  r0 - ascii char
+;   outputs:  r0 - hex char
+
+toHex
+	push{lr}
+	cmp r0, #57 ; highest number
+	suble r0, r0, #48 ; convert char 0-9 to 0-9
+	subgt r0, r0, #55 ; convert char A-F to A-F
+	pop {pc}
+	
+; subroutine Read
+;   Reads a message from uart
+;   Registers used:
+;	r0 - scratch
+;   inputs:  r1 - memory address to write to
+;   outputs:  none
+
+Read
+	push{r0, r1, lr}
+readLoop
+	bl Recieve		; get character from UART
+	cmp r0, #'\r'		; carriage return?
+	blne Transmit
+	strbne r0, [r1], #1	; store char, increment address
+	bne readLoop		; continue if not a '0'
+	mov r0, #0
+	strb r0, [r1]	; store null terminator
+	pop {r0, r1, pc}
+
+
+; subroutine Write
+;   Writes a message to uart
+;   Registers used:
+;	r0 - scratch register
+;   inputs:  r1 - address of start of message in memory
+;   outputs:  none
+
+Write
+	push{r0, r1, lr}
+writeLoop	
+	ldrb r0, [r1], #1	; load character, increment address
+	bl Transmit		; send character to UART
+	cmp r0, #0		; null terminated?
+	bne writeLoop		; continue if not a '0'
+	pop {r0, r1, pc}
+	
 ; subroutine UARTConfig
 ;   Configures the I/O pins first.
 ;   Then sets up the UART control register.
@@ -107,6 +236,12 @@ waitT	ldrb r6, [r5, #lsr0]	; get status of buffer
 	pop {r5, r6, pc}
 
 msgA dcb "Enter A: 0x\0"
-msgB dcb "Enter B: 0x\0"
-msgAdd dcb "A + B = 0x\0"
-	end
+msgB dcb " Enter B: 0x\0"
+msgAdd dcb " A + B = 0x\0"
+		
+	AREA myData, DATA, READWRITE
+	ALIGN
+hexA	dcb 0, 0, 0, 0, 0, 0, 0, 0, 0
+hexB	dcb 0, 0, 0, 0, 0, 0, 0, 0, 0
+hexSum	dcd 0
+	END
